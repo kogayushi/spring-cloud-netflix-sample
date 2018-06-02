02348132fca28ea934ef9f1908b9117a7d8d7424
kogayushi <youshe.old.pleasure@gmail.com>
2018-06-02 17:35:43 +0900

    integrate hystrix dashboard with turbin server

--- service-registry/src/main/java/com/example/netflix/RegistryServerApplication.java
+++ service-registry/src/main/java/com/example/netflix/RegistryServerApplication.java
@@ -3,9 +3,7 @@ package com.example.netflix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableHystrixDashboard
 @EnableEurekaServer
 @SpringBootApplication
 public class RegistryServerApplication  {
